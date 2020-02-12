1ef206d0e17a94c6549ee8cf1eff2fbe18cc2164
IegorSoboliev <evsobolev@gmail.com>
2020-02-12 17:47:57 +0200

    HW30 : implemented User on Spring

---
+++ src/main/java/spring/intro/service/UserService.java
@@ -0,0 +1,13 @@
+package spring.intro.service;
+
+import spring.intro.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    void add(User user);
+
+    List<User> listUsers();
+
+}
